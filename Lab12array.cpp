5f0fe96fbb451f42555a8989ba948eb7ba083bc2
talllguy <elliott.plack@gmail.com>
2013-11-19 16:50:29 -0500

    Set up
    initial program creation in VS2012

---
+++ Lab12array.cpp
@@ -0,0 +1,11 @@
+/************************************************************
+COSC 501
+Elliott Plack
+19 NOV 2013                                   Due 25 NOV 2013
+Problem: Create a 1-dimensional array with n elements; get
+	the size of the array as user input (validate!), max size
+	should be 10 (declared as class constant). Perform a
+	variety of functions with the array.
+Algorithm: Get array size from user, validate, get values,
+	perform functions.
+************************************************************/
