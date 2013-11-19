11072ccbf192a300086580f49a074686e43895b5
Elliott Plack <talllguy@users.noreply.github.com>
2013-11-19 18:08:57 -0500

    array declare
    while waiting for vs2013 to install, made a quick edit on github.com

--- Lab12array.cpp
+++ Lab12array.cpp
@@ -13,3 +13,14 @@ Algorithm: Get array size from user, validate, get values,
 using namespace std;
 
 int main ()
+{
+	int arraySize = 0;
+	
+	cout << "This program does a number of calculations on an array.\n"
+	     << "Enter the size of the array up to 10.";
+	while (cin >> arraySize <= 0 || arraySize > 10)
+	{
+		cin.clear();     // Clear the error flags 
+                cin.ignore(100, '\n');    // Remove unwanted characters from buffer 
+                cout << "\aEnter a positive integer, greater than 10: ";   // Re-issue the prompt 
+	}
